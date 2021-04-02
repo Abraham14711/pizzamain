edda61f5915f770e9bf9f0eef6421c6a0d851994
Abraham14711 <74873275+Abraham14711@users.noreply.github.com>
2021-04-02 15:03:14 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 from flask import Flask, render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
-from os import environ
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sqlite'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy()
-def create_app():
-    app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = environ['DATABASE_URL']
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
-    return app
+db.init_app(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(50),nullable=False)
-    tel=db.Column(db.String(11),nullable=False)
-    food=db.Column(db.String,nullable=False)
-    adress=db.Column(db.String(600),nullable=False)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    tel = db.Column(db.String(11), nullable=False)
+    food = db.Column(db.String, nullable=False)
+    adress = db.Column(db.String(600), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<users {self.id}>"
 
 @app.route('/')
 def main_page():
@@ -28,8 +30,9 @@ def send():
     tel=request.form['tel']
     food=request.form['food']
     adress=request.form['adress']
-    db.session.add(User(name,tel,food,adress))
-    return render_template('отправить.html')
+    db.session.add(Users(name,tel,food,adress))
+    db.session.commit()
+    return redirect(url_for('main page.html'))
 
 
 @app.route('/contacts')
@@ -39,7 +42,6 @@ def contacts():
 @app.route('/new')
 def new_things():
     return render_template('New things.html')
-
 
 @app.route('/new1')
 def new1():
